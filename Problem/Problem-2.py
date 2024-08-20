29b1337b16b229e173e006044dec9404b77b33b2
snowykte0426 <snowykte0426@naver.com>
2024-08-20 15:32:14 +0900

    20240820 Problem-2~3 수정

--- Problem/Problem-2.py
+++ Problem/Problem-2.py
@@ -1,4 +1,4 @@
-name = input("안녕하세요?\n이름이 어떻게 되시나요?")
+name = input("안녕하세요?\n이름이 어떻게 되시나요? ")
 print(f"{name}씨 만나서 반갑습니다. 당신의 이름길이는 {len(name)}이군요.")
-age = int(input("나이가 어떻게 되시나요?"))
+age = int(input("나이가 어떻게 되시나요? "))
 print(f"내년이면 {age + 1}세가 되시는군요.")
--- Problem/Problem-3.py
+++ Problem/Problem-3.py
@@ -1,3 +1,3 @@
-weight = input("몸무게를 kg 단위로 입력하시오.")
-height = input("키를 미터 단위로 입력하시오.")
+weight = input("몸무게를 kg 단위로 입력하시오. ")
+height = input("키를 미터 단위로 입력하시오. ")
 print(f"당신의 BMI는 {float(weight) / (float(height) ** 2)}입니다.")
