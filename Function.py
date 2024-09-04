cb021b8a7c09d2dea44baa1f99e8249443fa897a
snowykte0426 <snowykte0426@naver.com>
2024-09-04 14:54:50 +0900

    20240904

---
+++ Function.py
@@ -0,0 +1,9 @@
+def get_area(radius):
+    area = 3.14 * radius ** 2
+    return area
+
+
+result1 = get_area(3)
+print("반지름이 3인 원의 면적=", result1)
+result2 = get_area(20)
+print("반지름이 20인 원의 면적=", result2)
