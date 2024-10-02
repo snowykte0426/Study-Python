d42648e4282fc1d1b2376b2b0b22a681d202b339
snowykte0426 <snowykte0426@naver.com>
2024-10-02 10:30:37 +0900

    20241002 Problem-45~46

---
+++ Problem/Problem-45.py
@@ -0,0 +1,4 @@
+string1 = set(input())
+string2 = set(input())
+result = list(string1.intersection(string2))
+print(result)
---
+++ Problem/Problem-46.py
@@ -0,0 +1,4 @@
+string = input()
+word = set(string.split(" "))
+print(f"사용된 단어의 개수= {len(word)}")
+print(word)
