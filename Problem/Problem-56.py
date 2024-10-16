bac5feb0854fa0151ee9577162b68c1498512e8c
snowykte0426 <snowykte0426@naver.com>
2024-10-16 09:11:13 +0900

    20241016 Problem-56

---
+++ Problem/Problem-56.py
@@ -0,0 +1,39 @@
+values = [1, 2, 3, 4, 5, 6]
+print(values[1], "{values[1]}")
+print(values[-1], "{values[-1]}")
+print(values[1:3], "{values[1:3]}")
+print(len(values), "{len(values)}")
+print(values.index(5), "{values.index(5)}")
+print(max(values), "{max(values)}")
+print(sum(values), "{sum(values)}")
+print("------------------------")
+values = [0] * 10
+print(values)
+values = [x for x in range(1, 11)]
+print(values)
+values = [x for x in range(1, 11) if x % 2 == 0]
+print(values)
+values = [x * x for x in range(1, 11)]
+print(values)
+print("------------------------")
+aList = [10, 20, 30, 40, 50, 60]
+del aList[0:3]
+print(aList)
+print("------------------------")
+aList = [10, 20, 30, 40, 50, 60]
+print(aList[-2])
+print(aList[-3:-1])
+print("------------------------")
+aList.remove(60)
+print(aList[::-2])
+print("------------------------")
+aList = [x + y for x in ['Hello', 'Good'] for y in ['World', 'Bye']]
+print(aList)
+print("------------------------")
+aList = ['xyz', 'zz', 'PQRSTUVWZ']
+print(max(aList))
+print(min(aList))
+print("------------------------")
+aList = [1, 2, 3, 4]
+aList[1:4] = [5, 6, 7]
+print(aList)
