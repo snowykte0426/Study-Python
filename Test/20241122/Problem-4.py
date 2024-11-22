cd8c9fed646f3d155dac65525b812c7294fdf287
snowykte0426 <snowykte0426@naver.com>
2024-11-22 11:16:27 +0900

    20241122 수행평가 문제 풀이 수정

--- Test/20241122/Problem-4.py
+++ Test/20241122/Problem-4.py
@@ -1,5 +1,19 @@
 list1 = set(map(int, input("첫 번째 리스트를 입력하세요: ").split(",")))
 list2 = set(map(int, input("두 번째 리스트를 입력하세요: ").split(",")))
-print(f"교집합: {list1 & list2}")
-print(f"합집합: {list1 | list2}")
-print(f"차집합: {list1 - list2}")
+
+
+def intersection(set1, set2):
+    return set1 & set2
+
+
+def union(set1, set2):
+    return set1 | set2
+
+
+def difference(set1, set2):
+    return set1 - set2
+
+
+print(f"교집합: {intersection(list1, list2)}")
+print(f"합집합: {union(list1, list2)}")
+print(f"차집합: {difference(list1, list2)}")
